2c8329efac724247ba53d22716004b1fd5983be9
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2023-04-21 12:05:10 -0700

    changed settings from buttons to checkboxes

--- src/js/settings.js
+++ src/js/settings.js
@@ -3,7 +3,6 @@ let showStatus = true;
 function toggleStatus(newStatus) {
   showStatus = newStatus ?? !showStatus;
   setLocalStorage('status', showStatus.toString());
-  document.getElementById('status-selector').textContent = (showStatus ? 'Hide' : 'Show') + ' whether Correct or Incorrect';
 }
 
 let ignoreAccents = false;
@@ -11,5 +10,4 @@ let ignoreAccents = false;
 function toggleAccents(newAccents) {
   ignoreAccents = newAccents ?? !ignoreAccents;
   setLocalStorage('accents', ignoreAccents.toString());
-  document.getElementById('accents-selector').textContent = (ignoreAccents ? 'Check' : 'Ignore') + ' Accents';
 }
