5c67f5d7ea394e5fb73adc9a71930ca400b78e24
zayndamji <86502397+zayndamji@users.noreply.github.com>
2025-10-20 13:55:22 -0700

    infinitive set spanish 2

--- spanish2/js/cookie.js
+++ spanish2/js/cookie.js
@@ -55,8 +55,8 @@ if (tempWords != "" && tempWords != '[]' && setIsStillValid) {
   console.log('User has previous data... regenerating quiz from saved data.');
 } else {
   Array.from(document.getElementById('set').children).forEach(e => e.children[0].checked = false);
-  if (document.getElementById('unit1')) {
-    document.getElementById('unit1').checked = true;
+  if (document.getElementById('infinitive')) {
+    document.getElementById('infinitive').checked = true;
   }
   selectSet();
   console.log('User is new... generating quiz from scratch.');
