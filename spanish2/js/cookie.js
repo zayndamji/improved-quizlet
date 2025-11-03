60abdb0c5aecf967ce2ee455507d4ffad4cd0827
zayndamji <86502397+zayndamji@users.noreply.github.com>
2025-11-03 10:50:16 -0800

    la plaza mayor set

--- spanish2/js/cookie.js
+++ spanish2/js/cookie.js
@@ -55,8 +55,8 @@ if (tempWords != "" && tempWords != '[]' && setIsStillValid) {
   console.log('User has previous data... regenerating quiz from saved data.');
 } else {
   Array.from(document.getElementById('set').children).forEach(e => e.children[0].checked = false);
-  if (document.getElementById('infinitive')) {
-    document.getElementById('infinitive').checked = true;
+  if (document.getElementById('plaza')) {
+    document.getElementById('plaza').checked = true;
   }
   selectSet();
   console.log('User is new... generating quiz from scratch.');
