19097619e76da024422f39f96fd3e77e59e51878
zayndamji <86502397+zayndamji@users.noreply.github.com>
2026-01-14 09:41:47 -0800

    added quehaceres

--- spanish2/js/cookie.js
+++ spanish2/js/cookie.js
@@ -55,8 +55,8 @@ if (tempWords != "" && tempWords != '[]' && setIsStillValid) {
   console.log('User has previous data... regenerating quiz from saved data.');
 } else {
   Array.from(document.getElementById('set').children).forEach(e => e.children[0].checked = false);
-  if (document.getElementById('plaza')) {
-    document.getElementById('plaza').checked = true;
+  if (document.getElementById('quehaceres')) {
+    document.getElementById('quehaceres').checked = true;
   }
   selectSet();
   console.log('User is new... generating quiz from scratch.');
