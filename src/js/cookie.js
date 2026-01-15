2cbbf40b45ca95e865f3d211e5f7a6604819743e
zayndamji <86502397+zayndamji@users.noreply.github.com>
2026-01-15 08:30:17 -0800

    adjusted quiz 6 for new info

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -55,8 +55,8 @@ if (tempWords != "" && tempWords != '[]' && setIsStillValid) {
   console.log('User has previous data... regenerating quiz from saved data.');
 } else {
   Array.from(document.getElementById('set').children).forEach(e => e.children[0].checked = false);
-  if (document.getElementById('quiz6def')) {
-    document.getElementById('quiz6def').checked = true;
+  if (document.getElementById('quiz6spantospan')) {
+    document.getElementById('quiz6spantospan').checked = true;
   }
   selectSet();
   console.log('User is new... generating quiz from scratch.');
