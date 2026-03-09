0a566e054e65a812c5b2485b52ce1ec9c4c45176
zayndamji <86502397+zayndamji@users.noreply.github.com>
2026-03-09 10:19:29 -0700

    added imperfect spanish 2 set

--- spanish2/js/cookie.js
+++ spanish2/js/cookie.js
@@ -55,8 +55,8 @@ if (tempWords != "" && tempWords != '[]' && setIsStillValid) {
   console.log('User has previous data... regenerating quiz from saved data.');
 } else {
   Array.from(document.getElementById('set').children).forEach(e => e.children[0].checked = false);
-  if (document.getElementById('unidad3')) {
-    document.getElementById('unidad3').checked = true;
+  if (document.getElementById('imperfect')) {
+    document.getElementById('imperfect').checked = true;
   }
   selectSet();
   console.log('User is new... generating quiz from scratch.');
