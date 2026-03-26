516ee7b8fcd2440c9033c34c22c60abbdcf78934
zayndamji <86502397+zayndamji@users.noreply.github.com>
2026-03-26 09:48:49 -0700

    added quiz 9

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -55,11 +55,8 @@ if (tempWords != "" && tempWords != '[]' && setIsStillValid) {
   console.log('User has previous data... regenerating quiz from saved data.');
 } else {
   Array.from(document.getElementById('set').children).forEach(e => e.children[0].checked = false);
-  if (document.getElementById('quiz8gen')) {
-    document.getElementById('quiz8gen').checked = true;
-  }
-  if (document.getElementById('quiz8ref')) {
-    document.getElementById('quiz8ref').checked = true;
+  if (document.getElementById('quiz9')) {
+    document.getElementById('quiz9').checked = true;
   }
   selectSet();
   console.log('User is new... generating quiz from scratch.');
