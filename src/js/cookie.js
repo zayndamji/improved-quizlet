a98d72ec2aeb19596c00589fc270c4bff7d9e182
zayndamji <86502397+zayndamji@users.noreply.github.com>
2026-08-12 11:00:20 -0700

    change default choices for ap spanish sets

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -55,8 +55,14 @@ if (tempWords != "" && tempWords != '[]' && setIsStillValid) {
   console.log('User has previous data... regenerating quiz from saved data.');
 } else {
   Array.from(document.getElementById('set').children).forEach(e => e.children[0].checked = false);
-  if (document.getElementById('quiz9vocab')) {
-    document.getElementById('quiz9vocab').checked = true;
+  if (document.getElementById('untema')) {
+    document.getElementById('untema').checked = true;
+  }
+  if (document.getElementById('unaidea')) {
+    document.getElementById('unaidea').checked = true;
+  }
+  if (document.getElementById('elaborar')) {
+    document.getElementById('elaborar').checked = true;
   }
   selectSet();
   console.log('User is new... generating quiz from scratch.');
